39acaf02a95b3d7e14363abc418c2f73579bacdb
James Knighton <iamknighton@gmail.com>
2018-05-14 09:58:37 +0000

    Draw facial keypoints too.  Looks good.

--- faceit/look_at_dataset.py
+++ faceit/look_at_dataset.py
@@ -18,10 +18,16 @@ def parse_flags():
 def draw_sample(crop, info, f):
     im = crop.copy()
 
-    print(info[4:8])
     face_min_x, face_min_y, face_max_x, face_max_y = \
         map(lambda x: x.astype('int32').clip(0, 128), info[4:8])
     im[face_min_y:face_max_y, face_min_x:face_max_x, :] //= 2
+
+    for i in range(1, 22):
+        index = 8 + (i - 1) * 2
+        x = int(info[index])
+        y = int(info[index + 1])
+        if 1 <= x < 127 and 1 <= y < 127:
+            im[y - 1:y + 1, x - 1:x + 1, 0] = 255
 
     im = Image.fromarray(im)
     im.save(f)
@@ -31,8 +37,8 @@ def main(flags):
     assert not os.path.exists(flags.out_dir)
     os.makedirs(flags.out_dir)
     crops, infos = load_dataset(flags.in_dir)
-    print(crops.shape)
-    print(infos.shape)
+    print('crops:', crops.shape)
+    print('infos:', infos.shape)
     for i in range(len(crops)):
         f = '%d.jpg' % i
         f = os.path.join(flags.out_dir, f)
