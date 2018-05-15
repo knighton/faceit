ecc71361e66a325893ff93ea78516d16ba639ec4
James Knighton <iamknighton@gmail.com>
2018-05-15 04:18:03 -0700

    Rewrite annotate_video.

--- faceit/annotate_video.py
+++ faceit/annotate_video.py
@@ -17,19 +17,75 @@ tqdm.monitor_interval = 0
 def parse_flags():
     a = ArgumentParser()
 
-    # Model.
     a.add_argument('--model', type=str,
-                   default='data/checkpoints/epoch_0159_0149_0145/model.bin')
-    a.add_argument('--dim', type=int, default=128)
+                   default='data/checkpoints/epoch_0180_0114_0130/model.bin')
+    a.add_argument('--batch_size', type=int, default=32)
 
-    # Input video.
     a.add_argument('--in_video', type=str,
-                   default='/home/frak/dev/commaai/monitoring/video1/')
+                   default='/home/frak/dev/commaai/monitoring/video4/')
 
-    # Output video.
-    a.add_argument('--out_video', type=str, default='data/anno1/')
+    a.add_argument('--out_video', type=str, default='data/anno4/')
 
     return a.parse_args()
+
+
+def each_frame(video_dir, batch_size):
+    # get the list of frames.
+    pattern = os.path.join(video_dir, 'frame_*.jpg')
+    ff = glob(pattern)
+
+    # read frames from file.
+    crops = []
+    for i, f in tqdm(enumerate(ff), total=len(ff), leave=False):
+        # load the image and scale it down to a 128x128 thumbnail, dropping the
+        # left extreme to form a square (the driver is on the far right of
+        # image).
+        im = Image.open(f)
+        dim = 128 * 1152 / 864
+        im.thumbnail((dim, dim))
+        crop = np.array(im)
+        crop = crop[:, -128:, :]
+        crops.append(crop)
+    crops = np.stack(crops)
+
+    for i in range((len(crops) + batch_size - 1) // batch_size):
+        subcrops = crops[i * batch_size:(i + 1) * batch_size]
+        if 1 < len(subcrops):
+            yield subcrops
+
+
+def annotate_batch(model, crops):
+    # transform the crop to tensor.
+    x = crops.astype('float32')
+    x /= 127.5
+    x -= 1
+    x = x.transpose([0, 3, 1, 2])
+    x = torch.from_numpy(x).cuda()
+    xx = [x]
+
+    # Forward pass.
+    is_faces, is_males, poses, bboxes, keypoints = model.forward(xx)
+
+    # Draw the bounding box.
+    go = lambda x: x.detach().cpu().numpy()
+    for i in range(len(bboxes)):
+        min_x, min_y, max_x, max_y = map(int, go(bboxes[i]))
+        rect = crops[i, min_y:max_y, min_x:max_x, :]
+        confidence = int(256 * go(is_faces[i]))
+        crops[i, min_y:max_y, min_x:max_x, :] = \
+            np.maximum(rect, rect + confidence)
+
+    # Draw the eye points.
+    for i in range(len(bboxes)):
+        kp = keypoints[i]
+        x = int(kp[0])
+        y = int(kp[1])
+        crops[i, y - 1:y + 2, x - 1:x + 2, :] = 255
+        x = int(kp[2])
+        y = int(kp[3])
+        crops[i, y - 1:y + 2, x - 1:x + 2, :] = 255
+
+    return crops
 
 
 def main(flags):
@@ -38,44 +94,20 @@ def main(flags):
 
     model = torch.load(flags.model).cuda()
 
-    pattern = os.path.join(flags.in_video, 'frame_*.jpg')
-    ff = glob(pattern)
+    index = 0
+    for frame in each_frame(flags.in_video, flags.batch_size):
+        # Modify the frame to annotate bounding box, eyes, etc.
+        ims = annotate_batch(model, frame)
 
-    for i, f in tqdm(enumerate(ff), total=len(ff), leave=False):
-        im = Image.open(f)
-        dim = 128 * 1152 / 864
-        im.thumbnail((dim, dim))
-        crop = np.array(im)
-        crop = crop[:, -128:, :]
-
-        x = np.expand_dims(crop, 0)
-        x = x.astype('float32')
-        x /= 127.5
-        x -= 1
-        x = x.transpose([0, 3, 1, 2])
-        x = torch.from_numpy(x).cuda()
-        xx = [x]
-
-        is_faces, is_males, poses, bboxes, keypoints = model.forward(xx)
-        go = lambda x: x.detach().cpu().numpy()
-        min_x, min_y, max_x, max_y = map(int, go(bboxes[0]))
-        rect = crop[min_y:max_y, min_x:max_x, :]
-        confidence = int(256 * go(is_faces))
-        crop[min_y:max_y, min_x:max_x, :] = np.maximum(rect, rect + confidence)
-
-        keypoints = keypoints[0]
-        x = int(keypoints[0])
-        y = int(keypoints[1])
-        crop[y - 1:y + 2, x - 1:x + 2, :] = 255
-        x = int(keypoints[2])
-        y = int(keypoints[3])
-        crop[y - 1:y + 2, x - 1:x + 2, :] = 255
-
-        im = Image.fromarray(crop)
-        im = im.resize((256, 256))
-        f = 'anno_%05d.jpg' % i
-        f = os.path.join(flags.out_video, f)
-        im.save(f)
+        # Save to file.
+        for i in range(len(ims)):
+            im = ims[i]
+            im = Image.fromarray(im)
+            im = im.resize((256, 256))
+            f = 'anno_%05d.jpg' % index
+            f = os.path.join(flags.out_video, f)
+            im.save(f)
+            index += 1
 
 
 if __name__ == '__main__':
